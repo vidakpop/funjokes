301c446de6e824b6eee1b24a84f4fb6a31046814
vidakpop <daviskpop@gmail.com>
2025-07-26 05:33:32 +0300

    Fix resize_image function return statement and add pixel_to_ascii function

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ def resize_image(image, new_width=100):
     aspect_ratio = old_width /old_height
     new_height = int(new_width / aspect_ratio)
     return cv2.resize
+
+def pixel_to_ascii(pixel_value,ascii_chars):
+    return ascii_chars[pixel_value * len(ascii_chars) //256 ]
+
+
